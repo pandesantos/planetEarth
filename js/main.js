fdc812dfa8cd4451762b682d334233b266fd6582
Santosh <yasrpande@gmail.com>
2018-10-31 15:31:18 +0545

    modification

--- js/main.js
+++ js/main.js
@@ -2,16 +2,15 @@ var camera, scene, renderer;
 
 
 camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 100000 );
-camera.position.z = 20;
-camera.position.x = 5;
-camera.position.y = 2;
-
+camera.position.set(15, 5, 5);
 
 scene = new THREE.Scene();
 
-/** Creating a light source **/
-var light = new THREE.PointLight( 0xffffff,1,0,2);
-light.position.set(100,60,30);
+scene = new THREE.Scene();
+
+var light = new THREE.SpotLight(0xFFFFFF, 1, 0, Math.PI / 2, 1);
+light.position.set(4000, 4000, 1500);
+light.target.position.set (1000, 3800, 1000);
 scene.add(light);
 
 var loader = new THREE.TextureLoader();
@@ -31,7 +30,7 @@ space.geometry.mergeVertices();
 scene.add(space);
 
 /** creating earth **/
-var geoEarth = new THREE.SphereGeometry( 5, 32, 32 );
+var geoEarth = new THREE.SphereGeometry( 5, 40, 400 );
 // var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
 
 
@@ -42,7 +41,7 @@ var specular = loader.load('img/specular.jpg'); // shining texture
 var earthMaterial = new THREE.MeshPhongMaterial({
 	map: earthTexture,
 	bumpMap: bump,
-	bumpScale:0.08,
+	bumpScale:0.12,
 	specularMap: specular,
 });
 
@@ -50,7 +49,7 @@ var earthMaterial = new THREE.MeshPhongMaterial({
 var earth = new THREE.Mesh( geoEarth, earthMaterial ); // earth created
 scene.add( earth ); // earth added to the scene
 
-var geoCloud = new THREE.SphereGeometry(5.1,32,32); // cloud texture
+var geoCloud = new THREE.SphereGeometry(5.12,40,400); // cloud texture
 var textureCloud = loader.load('img/cloud.png');
 var cloudMaterial = new THREE.MeshPhongMaterial({
 	map: textureCloud,
@@ -68,13 +67,17 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var orbit = new THREE.OrbitControls( camera, renderer.domElement );
-
+orbit.enableZoom = false;
 /** create animation loop **/
 var animate = function animate(){
-	requestAnimationFrame( animate );
-	cloud.rotation.y += 0.0005;  
-	earth.rotation.y += 0.0005;
-	renderer.render( scene, camera );
+   requestAnimationFrame( animate );
+
+   var clock = new THREE.Clock(),
+   delta = clock.getDelta(); 
+
+   earth.rotation.y += 0.0005 ;
+   cloud.rotation.y += 0.0005 ;
+   renderer.render( scene, camera );
 }
 
 animate();
